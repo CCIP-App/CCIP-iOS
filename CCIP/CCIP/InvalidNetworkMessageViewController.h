3bb5f01f44c750e141e242311aa12c9b7bf02b90
腹黒い茶 <haraguroicha@hhmr.biz>
2016-06-25 23:09:19 +0800

    add viewcontroller and its class and xib

---
+++ CCIP/CCIP/InvalidNetworkMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  InvalidNetworkMessageViewController.h
+//  CCIP
+//
+//  Created by 腹黒い茶 on 2016/06/25.
+//  Copyright © 2016年 CPRTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InvalidNetworkMessageViewController : UIViewController
+
+@end
