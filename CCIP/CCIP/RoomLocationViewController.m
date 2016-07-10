34d01407813e2b66edf4dc456057c8da7b503a83
Frank Wu <frankwu100@gmail.com>
2016-07-10 20:44:39 +0800

    add UIColorFromRGB & reset ViewPagerIndicator color

--- CCIP/CCIP/RoomLocationViewController.m
+++ CCIP/CCIP/RoomLocationViewController.m
@@ -11,6 +11,18 @@
 #import "RoomProgramsTableViewController.h"
 #import "NSInvocation+addition.h"
 
+//RGB color macro
+#define UIColorFromRGB(rgbValue) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+//RGB color macro with alpha
+#define UIColorFromRGBWithAlpha(rgbValue,a) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
+
 @interface RoomLocationViewController () <ViewPagerDataSource, ViewPagerDelegate>
 
 @property (strong, nonatomic) AppDelegate *appDelegate;
@@ -137,7 +149,7 @@
 - (UIColor *)viewPager:(ViewPagerController *)viewPager colorForComponent:(ViewPagerComponent)component withDefault:(UIColor *)color {
     switch (component) {
         case ViewPagerIndicator: {
-            return [self.appDelegate.appArt secondaryColor];//[[UIColor redColor] colorWithAlphaComponent:0.64];
+            return UIColorFromRGB(0x517862);//[self.appDelegate.appArt primaryColor];//[[UIColor redColor] colorWithAlphaComponent:0.64];
         }
         case ViewPagerTabsView: {
             return [UIColor whiteColor];
