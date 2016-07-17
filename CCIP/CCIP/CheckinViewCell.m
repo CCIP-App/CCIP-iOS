0f2a6dc4adaeacca84aba5f9e1007859f00ac516
Sars <sars.tw@gmail.com>
2016-07-17 18:20:21 +0800

    Add alert dialog when using kit and lunch functions.

--- CCIP/CCIP/CheckinViewCell.m
+++ CCIP/CCIP/CheckinViewCell.m
@@ -19,12 +19,31 @@
 
 - (void)checkinBtnTouched {
     if ([self.id isEqualToString:@"day1checkin"] || [self.id isEqualToString:@"day2checkin"]) {
+        // TODO: Send API request
+        
         [self.checkinBtn setTitle:NSLocalizedString(@"CheckinViewButtonPressed", nil) forState:UIControlStateNormal];
+        [self.checkinBtn setBackgroundColor:[UIColor colorWithRed:155/255.0 green:155/255.0 blue:155/255.0 alpha:1]];
     } else {
-        [self.checkinBtn setTitle:NSLocalizedString(@"UseButtonPressed", nil) forState:UIControlStateNormal];
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"UseButton", nil) message:NSLocalizedString(@"ConfirmAlertText", nil) preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"CONFIRM" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+            // TODO: Send API request
+            
+            [self.checkinBtn setTitle:NSLocalizedString(@"UseButtonPressed", nil) forState:UIControlStateNormal];
+            [self.checkinBtn setBackgroundColor:[UIColor colorWithRed:155/255.0 green:155/255.0 blue:155/255.0 alpha:1]];
+        }];
+        
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+        
+        [alertController addAction:confirmAction];
+        [alertController addAction:cancelAction];
+        
+        UIViewController *topVC = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+        while (topVC.presentedViewController) {
+            topVC = topVC.presentedViewController;
+        }
+        [topVC presentViewController:alertController animated:YES completion:nil];
     }
-    
-    [self.checkinBtn setBackgroundColor:[UIColor colorWithRed:155/255.0 green:155/255.0 blue:155/255.0 alpha:1]];
 }
 
 @end
