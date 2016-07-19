c6d5c3b821759eaac8137e042dc788af76a96e60
Frank Wu <frankwu100@gmail.com>
2016-07-19 20:47:16 +0800

    Add Program's Abstract & SpeakerIntro ViewController

---
+++ CCIP/CCIP/ProgramSpeakerIntroViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProgramSpeakerIntroViewController.h
+//  CCIP
+//
+//  Created by FrankWu on 2016/7/19.
+//  Copyright © 2016年 CPRTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgramSpeakerIntroViewController : UIViewController
+
+@end
