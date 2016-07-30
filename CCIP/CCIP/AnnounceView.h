27fdfb201a51af78c484881e8ad8e42b1456f2b2
腹黒い茶 <haraguroicha@tipsy.coffee>
2016-07-31 01:29:42 +0800

    fix Announce View with blank class

---
+++ CCIP/CCIP/AnnounceView.h
@@ -0,0 +1,16 @@
+//
+//  AnnounceView.h
+//  CCIP
+//
+//  Created by 腹黒い茶 on 2016/07/31.
+//  Copyright © 2016年 CPRTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnnounceView : UIView
+
+@property (weak, nonatomic) IBOutlet UILabel *checkinMessabeLabel;
+@property (weak, nonatomic) IBOutlet UIButton *checkinBtn;
+
+@end
