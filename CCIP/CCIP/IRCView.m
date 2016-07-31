c0074ec6ffa35ad7a6e030f5311a9e3907d4909e
Frank Wu <frankwu100@gmail.com>
2016-07-31 23:16:08 +0800

    change UIActivityIndicatorView to UIRefreshControl on IRCView

--- CCIP/CCIP/IRCView.m
+++ CCIP/CCIP/IRCView.m
@@ -12,26 +12,51 @@
 
 @interface IRCView()
 
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
+
 @end
 
 @implementation IRCView
 
 - (void)drawRect:(CGRect)rect {
-    NSURL *nsurl = [NSURL URLWithString:LOG_BOT_URL];
-    NSURLRequest *requestObj = [NSURLRequest requestWithURL:nsurl];
     [self.webview setDelegate:self];
-    [self.webview loadRequest:requestObj];
+    [self.webview.scrollView setScrollEnabled:NO];
     
-    [self.activityIndicator startAnimating];
-    self.activityIndicator.hidesWhenStopped = YES;
+    [self.activityIndicator setHidden:YES];
     
     id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
     [tracker set:kGAIScreenName value:@"IRCView"];
     [tracker send:[[GAIDictionaryBuilder createScreenView] build]];
+    
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self
+                            action:@selector(refresh)
+                  forControlEvents:UIControlEventValueChanged];
+    [self.webview.scrollView addSubview:self.refreshControl];
+    
+    [self refresh];
+    [self.webview.scrollView setContentOffset:CGPointMake(0,-128) animated:NO];
+}
+
+- (void)refresh {
+    [self.refreshControl beginRefreshing];
+
+    NSURL *nsurl = self.webview.request.URL;
+    if (nsurl == nil) {
+        nsurl = [NSURL URLWithString:LOG_BOT_URL];
+        NSURLRequest *requestObj = [NSURLRequest requestWithURL:nsurl];
+        [self.webview loadRequest:requestObj];
+    }
+    else {
+        [self.webview reload];
+    }
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
-    [self.activityIndicator stopAnimating];
+    if (self.webview.scrollView.scrollEnabled == NO) {
+        [self.webview.scrollView setScrollEnabled:YES];
+    }
+    [self.refreshControl endRefreshing];
 }
 
 @end
