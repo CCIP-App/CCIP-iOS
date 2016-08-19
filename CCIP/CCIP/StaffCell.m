11cd6cfa0df75206cca0b8f9fbe327d4848aa515
腹黒い茶 <haraguroicha@tipsy.coffee>
2016-08-19 10:54:31 +0800

    freshen staff layout

--- CCIP/CCIP/StaffCell.m
+++ CCIP/CCIP/StaffCell.m
@@ -13,6 +13,14 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
+    self.backgroundColor = [UIColor whiteColor];
+    [self.layer setCornerRadius:5.0f];
+    [self.layer setBorderColor:[[UIColor lightGrayColor] CGColor]];
+    [self.layer setBorderWidth:0.3f];
+    [self.layer setShadowColor:[[UIColor grayColor] CGColor]];
+    [self.layer setShadowOpacity:0.3f];
+    [self.layer setShadowOffset:CGSizeMake(5.0f, 10.0f)];
+    [self.layer setMasksToBounds:NO];
 }
 
 @end
