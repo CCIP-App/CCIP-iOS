925807b4297559d3d1fb0923e6b7f5583b5a5fed
腹黒い茶 <haraguroicha@tipsy.coffee>
2017-07-18 02:47:53 +0800

    freshen to meet design
    TODO: add heart icon to every schedules and remove top navigation logo

--- CCIP/CCIP/ScheduleTableViewController.m
+++ CCIP/CCIP/ScheduleTableViewController.m
@@ -8,12 +8,19 @@
 
 #import "ScheduleTableViewController.h"
 #import "ScheduleTableViewCell.h"
+#import "UIColor+addition.h"
 
 @interface ScheduleTableViewController ()
+
+@property (strong, nonatomic) NSMutableArray *programTimes;
+@property (strong, nonatomic) NSMutableDictionary *programSections;
 
 @end
 
 @implementation ScheduleTableViewController
+
+static NSDateFormatter *formatter_full = nil;
+static NSDateFormatter *formatter_date = nil;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,6 +30,32 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    if (formatter_full == nil) {
+        formatter_full = [NSDateFormatter new];
+        [formatter_full setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
+    }
+    if (formatter_date == nil) {
+        formatter_date = [NSDateFormatter new];
+        [formatter_date setDateFormat:@"HH:mm"];
+        [formatter_date setTimeZone:[NSTimeZone timeZoneWithName:@"Asia/Taipei"]];
+    }
+    self.programTimes = [NSMutableArray new];
+    self.programSections = [NSMutableDictionary new];
+    for (NSDictionary *program in self.programs) {
+        NSDate *startTime = [formatter_full dateFromString:[program objectForKey:@"start"]];
+        NSString *start = [formatter_date stringFromDate:startTime];
+        NSMutableArray *section = [self.programSections objectForKey:start];
+        if (section == nil) {
+            section = [NSMutableArray new];
+            [self.programTimes addObject:startTime];
+        }
+        [section addObject:program];
+        [self.programSections setObject:section
+                                 forKey:start];
+    }
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"timeIntervalSince1970"
+                                                                   ascending:YES];
+    [self.programTimes sortUsingDescriptors:[NSArray arrayWithObject:sortDescriptor]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,6 +64,10 @@
 }
 
 #pragma mark - Table view data source
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 40.0f;
+}
+
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 80.0f;
 }
@@ -41,20 +78,26 @@
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return [self.programSections count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return [formatter_date stringFromDate:[self.programTimes objectAtIndex:section]];
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section {
+    UITableViewHeaderFooterView *header = (UITableViewHeaderFooterView *)view;
+    [header.textLabel setTextColor:[UIColor colorFromHtmlColor:@"#009A79"]];
+    [view setTintColor:[UIColor colorFromHtmlColor:@"#ECF5F4"]];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self.programs count];
+    NSDate *time = [self.programTimes objectAtIndex:section];
+    NSString *timeString = [formatter_date stringFromDate:time];
+    return [[self.programSections objectForKey:timeString] count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSDateFormatter *formatter_full = nil;
-    if (formatter_full == nil) {
-        formatter_full = [NSDateFormatter new];
-        [formatter_full setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
-    }
-
     NSString *scheduleCellName = @"ScheduleCell";
     
     ScheduleTableViewCell *cell = (ScheduleTableViewCell *)[tableView dequeueReusableCellWithIdentifier:scheduleCellName];
@@ -63,7 +106,9 @@
         cell = (ScheduleTableViewCell *)[tableView dequeueReusableCellWithIdentifier:scheduleCellName];
     }
     
-    NSDictionary *program = [self.programs objectAtIndex:indexPath.row];
+    NSDate *time = [self.programTimes objectAtIndex:indexPath.section];
+    NSString *timeString = [formatter_date stringFromDate:time];
+    NSDictionary *program = [[self.programSections objectForKey:timeString] objectAtIndex:indexPath.row];
     NSDate *startTime = [formatter_full dateFromString:[program objectForKey:@"start"]];
     NSDate *endTime = [formatter_full dateFromString:[program objectForKey:@"end"]];
     long mins = [[NSNumber numberWithDouble:([endTime timeIntervalSinceDate:startTime] / 60)] longValue];
