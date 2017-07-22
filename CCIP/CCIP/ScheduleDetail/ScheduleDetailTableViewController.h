104fd2fdf8ad0036704b48807e39abf3ffe182df
frankwu100 <frankwu100@gmail.com>
2017-07-22 21:22:41 +0800

    Add ScheduleDetailTableViewController

---
+++ CCIP/CCIP/ScheduleDetail/ScheduleDetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScheduleDetailTableViewController.h
+//  CCIP
+//
+//  Created by FrankWu on 2017/7/22.
+//  Copyright © 2017年 CPRTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScheduleDetailTableViewController : UITableViewController
+
+@end
