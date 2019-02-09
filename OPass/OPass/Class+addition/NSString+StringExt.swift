053a90611e85c96609927d10fba706b6682df266
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-02-09 18:00:27 +0800

    add string extensions for NSAttributedString and NSMutableAttributedString

--- OPass/OPass/Class+addition/NSString+StringExt.swift
+++ OPass/OPass/Class+addition/NSString+StringExt.swift
@@ -12,4 +12,65 @@ extension String {
     func appendingPathComponent(_ string: String) -> String {
         return URL(fileURLWithPath: self).appendingPathComponent(string).path
     }
+    func trim() -> String {
+        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+}
+
+extension Substring {
+    func trim() -> String {
+        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+}
+
+func + (left: NSAttributedString, right: NSAttributedString) -> NSAttributedString {
+    let result = NSMutableAttributedString()
+    result.append(left)
+    result.append(right)
+    return result
+}
+
+func + (left: String, right: NSAttributedString) -> NSAttributedString {
+    let result = NSMutableAttributedString()
+    result.append(NSAttributedString.init(string: left))
+    result.append(right)
+    return result
+}
+
+func + (left: NSAttributedString, right: String) -> NSAttributedString {
+    let result = NSMutableAttributedString()
+    result.append(left)
+    result.append(NSAttributedString.init(string: right))
+    return result
+}
+
+func + (left: String, right: NSMutableAttributedString) -> NSMutableAttributedString {
+    let result = NSMutableAttributedString()
+    result.append(NSAttributedString.init(string: left))
+    result.append(right)
+    return result
+}
+
+func + (left: NSMutableAttributedString, right: String) -> NSMutableAttributedString {
+    let result = NSMutableAttributedString()
+    result.append(left)
+    result.append(NSAttributedString.init(string: right))
+    return result
+}
+
+func += (left: inout NSMutableAttributedString, right: String) -> NSMutableAttributedString {
+    return NSMutableAttributedString.init(attributedString: NSAttributedString.init(attributedString: left) + right)
+}
+
+func += (left: inout NSAttributedString, right: String) -> NSAttributedString {
+    return left + right
+}
+
+func += (left: inout NSAttributedString, right: NSAttributedString) -> NSAttributedString {
+    return left + right
+}
+
+func += (left: inout NSAttributedString, right: NSAttributedString?) -> NSAttributedString {
+    guard let rhs = right else { return left }
+    return left += rhs
 }
