1d736f3fb789b9d82ad684774aeb8d8f8ac16910
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-03-03 00:00:28 +0800

    add blank OPass Events Controller as initial controller

---
+++ OPass/OPassEventsController.swift
@@ -0,0 +1,16 @@
+//
+//  OPassEventsController.swift
+//  OPass
+//
+//  Created by 腹黒い茶 on 2019/3/2.
+//  Copyright © 2019 OPass. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OPassEventsController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
