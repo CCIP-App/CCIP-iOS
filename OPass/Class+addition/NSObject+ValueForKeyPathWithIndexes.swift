14b427313d6af09d852632c7c2e5bec1f5f52f2b
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-03-09 00:02:49 +0800

    freshen code for prevent Swift deprecated warnings

--- OPass/Class+addition/NSObject+ValueForKeyPathWithIndexes.swift
+++ OPass/Class+addition/NSObject+ValueForKeyPathWithIndexes.swift
@@ -10,10 +10,10 @@ import Foundation
 
 @objc extension NSObject {
     func valueForKeyPathWithIndexes(_ fullPath: String) -> Any? {
-        return self.valueForKeyPaths(fullPath);
+        return self.valueForKeyPaths(fullPath)
     }
     func valueForKeyPaths(_ fullPath: String) -> Any? {
-        let testRange = fullPath.range(of: "[");
+        let testRange = fullPath.range(of: "[")
         if (testRange == nil) {
             if self.responds(to: Selector(fullPath)) {
                 return self.value(forKeyPath: fullPath) as Any
@@ -28,27 +28,27 @@ import Foundation
                 return nil
             }
         }
-        let parts = fullPath.components(separatedBy: ".");
-        var currentObj : NSObject? = self;
+        let parts = fullPath.components(separatedBy: ".")
+        var currentObj : NSObject? = self
         for part in parts {
-            let range1 = part.range(of: "[");
+            let range1 = part.range(of: "[")
             if (range1 == nil) {
-                currentObj = currentObj!.responds(to: Selector(part)) ? (currentObj!.value(forKey: part) as! NSObject) : nil;
+                currentObj = currentObj!.responds(to: Selector(part)) ? (currentObj!.value(forKey: part) as! NSObject) : nil
                 if (currentObj == nil) {
-                    return currentObj;
+                    return currentObj
                 }
             } else {
-                let range1End = String.Index(encodedOffset: range1!.lowerBound.encodedOffset);
-                let arrayKey = String(part[String.Index(encodedOffset: 0)..<range1End]);
-                let start = String.Index(encodedOffset: range1!.lowerBound.encodedOffset + 1)
-                let end = String.Index(encodedOffset: part.count - 1)
-                let index = Int(String(part[start..<end]));
-                currentObj = currentObj!.responds(to: Selector(arrayKey)) ? ((currentObj!.value(forKey: arrayKey) as! NSArray).object(at: index!) as! NSObject) : nil;
+                let range1End = String.Index(utf16Offset: range1!.lowerBound.utf16Offset(in: part), in: part)
+                let arrayKey = String(part[String.Index.init(utf16Offset: 0, in: "")..<range1End])
+                let start = String.Index(utf16Offset: range1!.lowerBound.utf16Offset(in: part) + 1, in: part)
+                let end = String.Index(utf16Offset: part.count - 1, in: part)
+                let index = Int(String(part[start..<end]))
+                currentObj = currentObj!.responds(to: Selector(arrayKey)) ? ((currentObj!.value(forKey: arrayKey) as! NSArray).object(at: index!) as! NSObject) : nil
                 if (currentObj == nil) {
-                    return currentObj;
+                    return currentObj
                 }
             }
         }
-        return currentObj;
+        return currentObj
     }
 }
--- OPass/Class+addition/UIColor+addition.swift
+++ OPass/Class+addition/UIColor+addition.swift
@@ -18,49 +18,49 @@ import UIKit
 
 @objc extension UIColor {
     static func colorFrom(_ from: UIColor, to: UIColor, at:Double) -> UIColor {
-        let f : CIColor = CIColor.init(cgColor: from.cgColor);
-        let t : CIColor = CIColor.init(cgColor: to.cgColor);
-        let resultRed = f.red + CGFloat(at) * (t.red - f.red);
-        let resultGreen = f.green + CGFloat(at) * (t.green - f.green);
-        let resultBlue = f.blue + CGFloat(at) * (t.blue - f.blue);
-        let resultAlpha = f.alpha + CGFloat(at) * (t.alpha - f.alpha);
-        return UIColor.init(red: resultRed, green: resultGreen, blue: resultBlue, alpha: resultAlpha);
+        let f : CIColor = CIColor.init(cgColor: from.cgColor)
+        let t : CIColor = CIColor.init(cgColor: to.cgColor)
+        let resultRed = f.red + CGFloat(at) * (t.red - f.red)
+        let resultGreen = f.green + CGFloat(at) * (t.green - f.green)
+        let resultBlue = f.blue + CGFloat(at) * (t.blue - f.blue)
+        let resultAlpha = f.alpha + CGFloat(at) * (t.alpha - f.alpha)
+        return UIColor.init(red: resultRed, green: resultGreen, blue: resultBlue, alpha: resultAlpha)
     }
 
     static func getColorByteFromHtmlColor(_ htmlColorString: String, forByte: UIColorByteMark) -> CGFloat {
-        assert(htmlColorString.hasPrefix("#"), "Must prefix begin with '#'");
-        let length = htmlColorString.count;
-        let hasAlpha = length == 9 || length == 5;
-        let isSingleByte = hasAlpha ? length == 5 : length == 4;
+        assert(htmlColorString.hasPrefix("#"), "Must prefix begin with '#'")
+        let length = htmlColorString.count
+        let hasAlpha = length == 9 || length == 5
+        let isSingleByte = hasAlpha ? length == 5 : length == 4
 
-        let byteLength = isSingleByte ? 1 : 2;
-        let startOffset = 1 + forByte.rawValue * byteLength - (hasAlpha ? 0 : forByte != .Alpha ? byteLength : 0);
-        let endOffset = startOffset + byteLength;
-        let startRange = String.Index(encodedOffset: startOffset);
-        let endRange = String.Index(encodedOffset: endOffset)
-        let byteString = String(htmlColorString[startRange..<endRange]);
+        let byteLength = isSingleByte ? 1 : 2
+        let startOffset = 1 + forByte.rawValue * byteLength - (hasAlpha ? 0 : forByte != .Alpha ? byteLength : 0)
+        let endOffset = startOffset + byteLength
+        let startRange = String.Index(utf16Offset: startOffset, in: htmlColorString)
+        let endRange = String.Index(utf16Offset: endOffset, in: htmlColorString)
+        let byteString = String(htmlColorString[startRange..<endRange])
         if (!hasAlpha && forByte == .Alpha) {
-            return CGFloat(self.hexToIntColor(String(repeating: "f", count: byteLength), isSingleByteOnly: isSingleByte));
+            return CGFloat(self.hexToIntColor(String(repeating: "f", count: byteLength), isSingleByteOnly: isSingleByte))
         }
-        return CGFloat(self.hexToIntColor(byteString, isSingleByteOnly: isSingleByte));
+        return CGFloat(self.hexToIntColor(byteString, isSingleByteOnly: isSingleByte))
     }
 
     static func colorFromHtmlColor(_ htmlColorString: String) -> UIColor {
-        let r = self.getColorByteFromHtmlColor(htmlColorString, forByte: .Red);
-        let g = self.getColorByteFromHtmlColor(htmlColorString, forByte: .Green);
-        let b = self.getColorByteFromHtmlColor(htmlColorString, forByte: .Blue);
-        let a = self.getColorByteFromHtmlColor(htmlColorString, forByte: .Alpha);
-        return UIColor.init(red: r, green: g, blue: b, alpha: a);
+        let r = self.getColorByteFromHtmlColor(htmlColorString, forByte: .Red)
+        let g = self.getColorByteFromHtmlColor(htmlColorString, forByte: .Green)
+        let b = self.getColorByteFromHtmlColor(htmlColorString, forByte: .Blue)
+        let a = self.getColorByteFromHtmlColor(htmlColorString, forByte: .Alpha)
+        return UIColor.init(red: r, green: g, blue: b, alpha: a)
     }
 
     static func hexToIntColor(_ hex : String, isSingleByteOnly:Bool) -> Float {
-        var h : String = hex;
+        var h : String = hex
         if (isSingleByteOnly) {
-            h = h.appending(hex);
+            h = h.appending(hex)
         }
-        var result : CUnsignedInt = 0;
-        let scanner : Scanner = Scanner(string: h);
-        scanner.scanHexInt32(&result);
-        return Float(result) / 255.0;
+        var result : CUnsignedInt = 0
+        let scanner : Scanner = Scanner(string: h)
+        scanner.scanHexInt32(&result)
+        return Float(result) / 255.0
     }
 }
