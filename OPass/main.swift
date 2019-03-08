c98e55a90ac27f4e931e7f3a9d9c8095ff39c202
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-03-09 03:54:13 +0800

    freshen main.m to main.swift

---
+++ OPass/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  OPass
+//
+//  Created by 腹黒い茶 on 2016/06/24.
+//  Copyright © 2016年 OPass. All rights reserved.
+//
+
+import UIKit
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    NSStringFromClass(UIApplication.self),
+    NSStringFromClass(AppDelegate.self)
+)
