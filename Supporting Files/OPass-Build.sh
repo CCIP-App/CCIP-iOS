01e1236fb1d8301092e14991e245ad60de0bfd40
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-03-11 03:44:32 +0800

    freshen build script with auto build version

--- Supporting Files/OPass-Build.sh
+++ Supporting Files/OPass-Build.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 export MAIN_VERSION=3.0
+export GIT_COMMITS=$(($(git rev-list --all --count) + 1))
+export SETTINGS_FILE="${PROJECT_DIR}/Supporting Files/Settings.bundle/Root.plist"
+export SETTINGS_FILE_TEMPLATE="${PROJECT_DIR}/Supporting Files/Settings.bundle/Root-blank.plist"
+export APP_FLAGS_FILE="${PROJECT_DIR}/Supporting Files/Settings.bundle/AppFlags.plist"
+
+cp "${SETTINGS_FILE_TEMPLATE}" "${SETTINGS_FILE}"
 
 trim() {
     local var=$@
@@ -9,7 +17,7 @@ trim() {
 settingBundle() {
     local keyValue=$1
     local value=$2
-    _cnt=`/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:" "$SETTINGS_FILE"|grep Dict|wc -l`
+    _cnt=`/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:" "$SETTINGS_FILE" | grep Dict | wc -l`
     for idx in `seq 0 $(($_cnt - 1))`; do
         # echo "the index is: $idx."
         val=`/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:$idx:Key" "$SETTINGS_FILE"`
@@ -56,12 +64,11 @@ buildVersion=`agvtool vers -terse`
 cd "$PROJECT_DIR"
 productVersion=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$INFOPLIST_FILE"`
 productVersion="$MAIN_VERSION"
-mainVersion="$productVersion.$buildVersion"
+#mainVersion="$productVersion.$buildVersion"
+mainVersion="$productVersion.$GIT_COMMITS"
 version="$mainVersion #$gitVersion"
 flags=$GCC_PREPROCESSOR_DEFINITIONS
 timeStamp=`date +"%Y-%m-%d %H:%M:%S GMT+8"`
-SETTINGS_FILE=Settings.bundle/Root.plist
-APP_FLAGS_FILE=Settings.bundle/AppFlags.plist
 
 # /usr/libexec/PlistBuddy -c "set CFBundleVersion $version" "$INFOPLIST_FILE"
 
@@ -70,8 +77,8 @@ settingBundle "version_preference" "$version $ARCHS";
 settingBundle "build_timestamp" "$timeStamp";
 settingFlagsBundle $flags;
 
-cat CCIP.xcconfig > CCIP.debug.xcconfig
-cat CCIP.xcconfig > CCIP.release.xcconfig
+cat "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.xcconfig" > "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.debug.xcconfig"
+cat "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.xcconfig" > "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.release.xcconfig"
 
 export CONF_NAME="`echo $TARGET_NAME | awk '{ print tolower($0) }'`"
 export domain="$TARGET_NAME"
@@ -83,8 +90,10 @@ function replace_slash() {
 function xcc_replace() {
     f=`replace_slash "$2"`
     r=`replace_slash "$3"`
-    sed -i '' -E 's/'"$f"'/'"$r"'/g' CCIP.debug.xcconfig
-    [[ "$1" -eq "0" ]] && sed -i '' -E 's/'"$f"'/'"$r"'/g' CCIP.release.xcconfig || sed -i '' -E 's/'"$f"'//g' CCIP.release.xcconfig
+    sed -i '' -E 's/'"$f"'/'"$r"'/g' "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.debug.xcconfig"
+    [[ "$1" -eq "0" ]] && \
+        sed -i '' -E 's/'"$f"'/'"$r"'/g' "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.release.xcconfig" || \
+        sed -i '' -E 's/'"$f"'//g' "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.release.xcconfig"
 }
 
 #xcc_replace 1 "#Dev#"                       "-Dev"
@@ -95,3 +104,4 @@ xcc_replace 0 "#APP_NAME#"                  "$TARGET_NAME"
 xcc_replace 0 "#define#"                    "$PREDEFINITIONS"
 xcc_replace 0 "#domain#"                    "$domain"
 xcc_replace 0 "#CONF_NAME#"                 "$CONF_NAME"
+xcc_replace 0 "#GIT_COMMITS#"               "$GIT_COMMITS"
