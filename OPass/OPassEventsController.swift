c2f6891bb69867fd5d2f7da7471fae81802a6813
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-03-11 02:51:44 +0800

    remove useless code

--- OPass/OPassEventsController.swift
+++ OPass/OPassEventsController.swift
@@ -109,34 +109,11 @@ class OPassEventsController : UIViewController, UITableViewDelegate, UITableView
 
     func tableView(_: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
-//        return cellHeights[indexPath.row]
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath) as! OPassEventCell
         let _ = self.LoadEvent(cell.EventId)
-//        let cell = tableView.cellForRow(at: indexPath) as! FoldingCell
-//
-//        if cell.isAnimating() {
-//            return
-//        }
-//
-//        var duration = 0.0
-//        let cellIsCollapsed = cellHeights[indexPath.row] == Const.closeCellHeight
-//        if cellIsCollapsed {
-//            cellHeights[indexPath.row] = Const.openCellHeight
-//            cell.unfold(true, animated: true, completion: nil)
-//            duration = 0.5
-//        } else {
-//            cellHeights[indexPath.row] = Const.closeCellHeight
-//            cell.unfold(false, animated: true, completion: nil)
-//            duration = 0.8
-//        }
-//
-//        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { () -> Void in
-//            tableView.beginUpdates()
-//            tableView.endUpdates()
-//        }, completion: nil)
     }
 
 }
