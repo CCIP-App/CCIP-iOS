931735c2df65738a0e6ba8c219c4b3eff9355800
Frank Wu <frankwu100@gmail.com>
2019-07-28 01:15:15 +0800

    modify EventCell's color setting

--- OPass/OPassEventCell.swift
+++ OPass/OPassEventCell.swift
@@ -18,6 +18,26 @@ class OPassEventCell: FoldingCell {
     override func awakeFromNib() {
         foregroundView.layer.cornerRadius = 10
         self.backgroundColor = UIColor.clear
+
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowRadius = 4.0
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowOffset = CGSize(width: 1, height: 1)
+        self.layer.masksToBounds = false
+
+        self.EventLogo.layer.shadowColor = UIColor.black.cgColor
+        self.EventLogo.layer.shadowRadius = 4.0
+        self.EventLogo.layer.shadowOpacity = 0.3
+        self.EventLogo.layer.shadowOffset = CGSize(width: 1, height: 1)
+        self.EventLogo.layer.masksToBounds = false
+
+        self.EventName.textColor = .white
+        self.EventName.layer.shadowColor = UIColor.black.cgColor
+        self.EventName.layer.shadowRadius = 4.0
+        self.EventName.layer.shadowOpacity = 0.3
+        self.EventName.layer.shadowOffset = CGSize(width: 1, height: 1)
+        self.EventName.layer.masksToBounds = false
+
         super.awakeFromNib()
     }
 
