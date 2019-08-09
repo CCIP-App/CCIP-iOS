9f05621a43662dfc1bb91481a3fa363ced16fc12
secminhr <secminhrian@gmail.com>
2019-08-09 18:29:37 +0800

    indent issue in InvalidNetworkRetryDelegate fixed

--- OPass/Tabs/CheckinTab/CheckinOverlay/InvalidNetworkRetryDelegate.swift
+++ OPass/Tabs/CheckinTab/CheckinOverlay/InvalidNetworkRetryDelegate.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 @objc protocol InvalidNetworkRetryDelegate {
-	@objc var controllerTopStart: CGFloat { get set }
+    @objc var controllerTopStart: CGFloat { get set }
     @objc optional func refresh()
 }
