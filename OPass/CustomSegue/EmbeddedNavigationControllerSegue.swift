319e5d2149ac03cf725e58b88c973bbfbf4a0687
Frank Wu <frankwu100@gmail.com>
2019-08-18 11:04:30 +0800

    fine tune all iPhone's layout style as the iPhone5's

--- OPass/CustomSegue/EmbeddedNavigationControllerSegue.swift
+++ OPass/CustomSegue/EmbeddedNavigationControllerSegue.swift
@@ -17,9 +17,9 @@ class EmbeddedNavigationControllerSegue: UIStoryboardSegue {
         let screenWidth = UIScreen.main.bounds.width
         let screenHeight = UIScreen.main.bounds.height
 
-        let navBarHeight = (self.source.navigationController?.navigationBar.frame.size.height)!
-        let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
-        let tabBarHeight = (self.source.tabBarController?.tabBar.frame.size.height)!
+//        let navBarHeight = (self.source.navigationController?.navigationBar.frame.size.height)!
+//        let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
+//        let tabBarHeight = (self.source.tabBarController?.tabBar.frame.size.height)!
 
         var frameHeight : CGFloat
         var superViewHeight : CGFloat
@@ -27,16 +27,16 @@ class EmbeddedNavigationControllerSegue: UIStoryboardSegue {
 
         var frame : CGRect
 
-        switch UIDevice.current.deviceType {
-        case .iPhoneSE, .iPhone5C, .iPhone5S, .simulator:
+//        switch UIDevice.current.deviceType {
+//        case .iPhoneSE, .iPhone5C, .iPhone5S:
             frameHeight = screenHeight
             superViewHeight = screenHeight
             superViewTop = 0
-        default:
-            frameHeight = screenHeight - (statusBarHeight + navBarHeight)
-            superViewHeight = screenHeight - (statusBarHeight + navBarHeight + tabBarHeight)
-            superViewTop = statusBarHeight + navBarHeight
-        }
+//        default:
+//            frameHeight = screenHeight - (statusBarHeight + navBarHeight)
+//            superViewHeight = screenHeight - (statusBarHeight + navBarHeight + tabBarHeight)
+//            superViewTop = statusBarHeight + navBarHeight
+//        }
 
         frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: screenWidth, height: frameHeight))
         destinationView.frame = frame.offsetBy(dx: 0, dy: screenHeight)
