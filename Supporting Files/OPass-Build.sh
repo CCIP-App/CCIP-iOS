205fadeae871f09bfc93a23a5931babf57f8eea1
腹黒い茶 <haraguroicha@tipsy.coffee>
2020-09-14 21:56:10 +0800

    fix for improve code factor for build script

--- Supporting Files/OPass-Build.sh
+++ Supporting Files/OPass-Build.sh
@@ -10,7 +10,7 @@ export APP_FLAGS_FILE="${SETTINGS_BUNDLE}/AppFlags.plist"
 cp "${SETTINGS_FILE_TEMPLATE}" "${SETTINGS_FILE}"
 
 trim() {
-    local var=$@
+    local var=( "$@" )
     var="${var#"${var%%[![:space:]]*}"}"   # remove leading whitespace characters
     var="${var%"${var##*[![:space:]]}"}"   # remove trailing whitespace characters
     echo -n "$var"
@@ -18,11 +18,11 @@ trim() {
 settingBundle() {
     local keyValue=$1
     local value=$2
-    _cnt=`/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:" "$SETTINGS_FILE" | grep Dict | wc -l`
-    for idx in `seq 0 $(($_cnt - 1))`; do
+    _cnt=$(/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:" "$SETTINGS_FILE" | grep Dict | wc -l)
+    for idx in $(seq 0 $(($_cnt - 1))); do
         # echo "the index is: $idx."
-        val=`/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:$idx:Key" "$SETTINGS_FILE"`
-        val=`trim "$val"`;
+        val=$(/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:$idx:Key" "$SETTINGS_FILE")
+        val=$(trim "$val")
         # echo "the value of PreferenceSpecifiers:${idx}:Key: is ${val}."
 
         if [ "$val" == "$keyValue" ]; then
@@ -31,7 +31,7 @@ settingBundle() {
             /usr/libexec/PlistBuddy -c "Set PreferenceSpecifiers:$idx:DefaultValue $value" "$SETTINGS_FILE"
 
             # just to be sure that it worked
-            ver=`/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:$idx:DefaultValue" "$SETTINGS_FILE"`
+            ver=$(/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:$idx:DefaultValue" "$SETTINGS_FILE")
             echo "PreferenceSpecifiers:$idx:DefaultValue set to: $ver"
         fi
     done
@@ -52,36 +52,36 @@ settingFlagsBundle() {
     /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:0:Type string ""PSGroupSpecifier""" "$APP_FLAGS_FILE"
     /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:0:Title string ""Application Flags""" "$APP_FLAGS_FILE"
     until [ "$#" == "0" ]; do
-        defineString=`echo $1 | sed -e 's/=/ /'`
-        defineKey=`echo $defineString | awk '{print $1}'`
-        defineValue=`echo $defineString | sed -e 's/'$defineKey' //'`
-        settingFlagsDefine "$defineKey" "$defineValue";
+        defineString=$(echo $1 | sed -e 's/=/ /')
+        defineKey=$(echo $defineString | awk '{print $1}')
+        defineValue=$(echo $defineString | sed -e 's/'$defineKey' //')
+        settingFlagsDefine "$defineKey" "$defineValue"
         shift
     done
     /usr/libexec/PlistBuddy -c "Add :Title string ""SettingsTitle""" "$APP_FLAGS_FILE"
 }
-gitVersion=`git rev-parse --short HEAD`
-buildVersion=`agvtool vers -terse`
+gitVersion=$(git rev-parse --short HEAD)
+buildVersion=$(agvtool vers -terse)
 cd "$PROJECT_DIR"
-productVersion=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$INFOPLIST_FILE"`
+productVersion=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$INFOPLIST_FILE")
 productVersion="$MAIN_VERSION"
 #mainVersion="$productVersion.$buildVersion"
 mainVersion="$productVersion.$GIT_COMMITS"
 version="$mainVersion #$gitVersion"
 flags=$GCC_PREPROCESSOR_DEFINITIONS
-timeStamp=`date +"%Y-%m-%d %H:%M:%S GMT+8"`
+timeStamp=$(date +"%Y-%m-%d %H:%M:%S GMT+8")
 
 # /usr/libexec/PlistBuddy -c "set CFBundleVersion $version" "$INFOPLIST_FILE"
 
 cd "Supporting Files"
-settingBundle "version_preference" "$version $ARCHS";
-settingBundle "build_timestamp" "$timeStamp";
-settingFlagsBundle $flags;
+settingBundle "version_preference" "$version $ARCHS"
+settingBundle "build_timestamp" "$timeStamp"
+settingFlagsBundle $flags
 
 cat "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.xcconfig" > "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.debug.xcconfig"
 cat "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.xcconfig" > "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.release.xcconfig"
 
-export CONF_NAME="`echo $TARGET_NAME | awk '{ print tolower($0) }'`"
+export CONF_NAME="$(echo $TARGET_NAME | awk '{ print tolower($0) }')"
 export domain="$TARGET_NAME"
 
 function replace_slash() {
@@ -89,8 +89,8 @@ function replace_slash() {
 }
 
 function xcc_replace() {
-    f=`replace_slash "$2"`
-    r=`replace_slash "$3"`
+    f=$(replace_slash "$2")
+    r=$(replace_slash "$3")
     sed -i '' -E 's/'"$f"'/'"$r"'/g' "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.debug.xcconfig"
     [[ "$1" -eq "0" ]] && \
         sed -i '' -E 's/'"$f"'/'"$r"'/g' "${PROJECT_DIR}/Supporting Files/${TARGET_NAME}.release.xcconfig" || \
