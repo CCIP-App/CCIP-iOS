5d747a2bebdb8c8afc6cf18ed49728cab706119f
viere1234 <69096696+viere1234@users.noreply.github.com>
2022-02-28 22:47:06 +0800

    First

---
+++ OPass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OPass
+//
+//  Created by 張智堯 on 2022/2/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OPass/OPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  OPassApp.swift
+//  OPass
+//
+//  Created by 張智堯 on 2022/2/28.
+//
+
+import SwiftUI
+
+@main
+struct OPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
