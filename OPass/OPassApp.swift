8a732f0d2437b776d6f8fbd9fe69ad4b2cab6395
secminhr <secminhrian@gmail.com>
2022-03-06 21:41:25 +0800

    Setup and configure Firebase.

--- OPass/OPassApp.swift
+++ OPass/OPassApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct OPassApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
