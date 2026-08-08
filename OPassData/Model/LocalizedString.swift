3e18ee4f39f6b361d2297c9ebfb3b46e9133b5e4
Brian Chang <brianchang928@gmail.com>
2026-08-08 23:12:03 +0800

    feat: Add more optional languages to LocalizedString
    Signed-off-by: Brian Chang <brianchang928@gmail.com>

--- OPassData/Model/LocalizedString.swift
+++ OPassData/Model/LocalizedString.swift
@@ -3,14 +3,20 @@
 //  OPass
 //
 //  Created by Brian Chang on 2023/7/30.
-//  2025 OPass.
+//  2026 OPass.
 //
 
 import Foundation
 
 struct LocalizedString: Hashable, Codable, Localizable {
-    var zh: String
     var en: String
+    var zh: String
+    var hi: String?
+    var ja: String?
+    var ko: String?
+    var nan: String?
+    var nb: String?
+    var ta: String?
 }
 
 struct LocalizedCodeString: Hashable, Codable, Localizable {
